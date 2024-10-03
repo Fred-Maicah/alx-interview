0197338e4b2109b3ed70c6f423cb47a0d8de511c
fred maicah <fredmaicah@gmail.com>
2024-10-03 22:03:54 +0000

    My 0x00. Pascal's Triangle commit

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,23 @@
+#!/usr/bin/python3
+"""
+0pascal_triangle
+
+"""
 def pascal_triangle(n):
+    """
+    Generates Pascal's Triangle up to n rows.
+    Returns a list of lists of integers.
+    """
     if n <= 0:
         return []
     
-    triangle = [[1]]  # First row of Pascal's triangle is always [1]
+    triangle = [[1]]  # First row is always [1]
     
     for i in range(1, n):
         row = [1]  # Each row starts with 1
         prev_row = triangle[i - 1]
         
-        # Calculate the values in between the first and last element of the row
+        # Generate the interior elements of the row
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
         
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,2 @@
+
+This is the readme file for 0x00. Pascal's Triangle project.
